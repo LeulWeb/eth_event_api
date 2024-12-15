cbac85363b35c9bc8700bc4677c16910ff926ae9
leul <leulwebsdev@gmail.com>
2024-12-15 21:11:30 +0300

    eth event project setup

---
+++ src/main/java/com/bishu/eth_event/EthEventApplication.java
@@ -0,0 +1,13 @@
+package com.bishu.eth_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bishu/eth_event/EthEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bishu.eth_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
